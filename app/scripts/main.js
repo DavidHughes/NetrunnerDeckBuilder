93fc922cd1b3df96177360c6616d798db49854b7
David Hughes <david.hughes125@gmail.com>
2015-02-05 20:27:05 +0000

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,12 @@
+'use strict';
+var deckBuilder = angular.module('deckBuilder', []);
+
+deckBuilder.controller('DeckBuilderCtrl', ['$scope', '$http', function($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'data/allCards.json'
+    }).success(function(data) {
+        $scope.allCards = data.netrunnerCards;
+        console.log(data.netrunnerCards);
+    });
+}]);
