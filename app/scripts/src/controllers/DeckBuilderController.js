30da6c71329b5d98bc537e6e669edc840e8fc6be
David Hughes <david.hughes125@gmail.com>
2015-02-07 14:18:01 +0000

    Add plus/minus buttons & quantity counter

--- app/scripts/src/controllers/DeckBuilderController.js
+++ app/scripts/src/controllers/DeckBuilderController.js
@@ -11,11 +11,28 @@
       var newQuantity = 1;
 
       if ($scope.deckStatus[card.code]) {
-        newQuantity += $scope.deckStatus[card.code].quantity;
+        newQuantity = $scope.deckStatus[card.code].quantity + 1;
       }
 
       if (newQuantity > 3) {
         newQuantity = 3;
+      }
+
+      $scope.deckStatus[card.code] = {
+        card: card,
+        quantity: newQuantity
+      };
+    };
+
+    $scope.removeCard = function(card) {
+      var newQuantity = 0;
+
+      if ($scope.deckStatus[card.code]) {
+        newQuantity = $scope.deckStatus[card.code].quantity - 1;
+      }
+
+      if (newQuantity < 0) {
+        newQuantity = 0;
       }
 
       $scope.deckStatus[card.code] = {
