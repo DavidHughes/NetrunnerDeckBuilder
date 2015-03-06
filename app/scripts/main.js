95609cf0950a98274264ae822b5f7de3ae18b00f
David Hughes <david.hughes125@gmail.com>
2015-03-06 22:11:54 +0000

    Goodbye #/

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,21 +10,23 @@
 
 (function() {
   'use strict';
-  angular.module('deckBuilder').config(['$routeProvider', function($routeProvider){
-  $routeProvider
-    .when('/', {
-      controller: 'DeckManagerController',
-      templateUrl: 'templates/deck-manager.html'
-    }).
-    when('/deck/edit/:deckId', {
-      controller: 'DeckBuilderController',
-      templateUrl: 'templates/deck-builder.html',
-      resolve: {
-        'AllCardsService': function(AllCardsService) {
-          return AllCardsService;
+  angular.module('deckBuilder').config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    $routeProvider
+      .when('/', {
+        controller: 'DeckManagerController',
+        templateUrl: 'templates/deck-manager.html'
+      }).
+      when('/deck/edit/:deckId', {
+        controller: 'DeckBuilderController',
+        templateUrl: 'templates/deck-builder.html',
+        resolve: {
+          'AllCardsService': function(AllCardsService) {
+            return AllCardsService;
+          }
         }
-      }
-    });
+      });
+
+    $locationProvider.html5Mode(true);
   }]);
 })();
 
