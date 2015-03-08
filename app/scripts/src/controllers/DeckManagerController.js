92a19e61cfcdac65457a1c0d8d9ab7f17cbe31b2
David Hughes <david.hughes125@gmail.com>
2015-03-08 22:19:10 +0000

    Add deck renaming functionality to deck manager view.

--- app/scripts/src/controllers/DeckManagerController.js
+++ app/scripts/src/controllers/DeckManagerController.js
@@ -3,9 +3,12 @@
   angular.module('deckBuilder').controller('DeckManagerController', ['$scope', 'UserDecksService', function($scope, UserDecksService) {
     $scope.allDecks = UserDecksService.getDecks();
 
+    $scope.changeDeckName = null;
+
     $scope.setDeckName = function(deckId, newName) {
       if ($scope.allDecks[deckId]) {
         $scope.allDecks[deckId].name = newName;
+        UserDecksService.saveDeck($scope.allDecks[deckId]);
       }
     };
   }]);
