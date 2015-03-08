d067c288d73bae0025e61c7a07f4ba1b42af8d21
David Hughes <david.hughes125@gmail.com>
2015-03-08 13:05:25 +0000

    Add DeckManagerController#setDeckName

--- app/scripts/src/controllers/DeckManagerController.js
+++ app/scripts/src/controllers/DeckManagerController.js
@@ -2,5 +2,11 @@
   'use strict';
   angular.module('deckBuilder').controller('DeckManagerController', ['$scope', 'UserDecksService', function($scope, UserDecksService) {
     $scope.allDecks = UserDecksService.getDecks();
+
+    $scope.setDeckName = function(deckId, newName) {
+      if ($scope.allDecks[deckId]) {
+        $scope.allDecks[deckId].name = newName;
+      }
+    };
   }]);
 })();
---
+++ app/scripts/tests/controllers/DeckManagerController.js
@@ -0,0 +1,38 @@
+function initialiseStorage() {
+  localStorage.setItem('allDecks', JSON.stringify({
+    1: {
+      card: {},
+      totalCards: 0,
+      agendaPoints: 0,
+      requiredAgendaPoints: [18, 19],
+      name: 'Blank Test Deck',
+      id: 1
+    }
+  }));
+}
+initialiseStorage();
+
+describe('DeckManagerController', function() {
+  var scope;
+  beforeEach(function() {
+    module('deckBuilder');
+    inject(function($rootScope, $controller) {
+      scope = $rootScope.$new();
+      $controller('DeckManagerController', {
+        $scope: scope
+      });
+    });
+
+    initialiseStorage();
+  });
+
+  it('can change the name of a deck', function() {
+    var newName = 'Test Name Change';
+    scope.setDeckName(1, newName);
+
+    expect(scope.allDecks[1].name).to.equal(newName);
+
+    scope.setDeckName(2, 'Deck does not exist');
+    // Should not error - chai will automatically fail at this point.
+  });
+});
