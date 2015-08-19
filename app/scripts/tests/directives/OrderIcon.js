e8d71b7010756caf3a5eeea68a88e38f8d9d331f
David Hughes <david.hughes125@gmail.com>
2015-08-19 13:35:31 +0100

    tests: Improve testing for order-icon.

--- app/scripts/tests/directives/OrderIcon.js
+++ app/scripts/tests/directives/OrderIcon.js
@@ -1,20 +1,22 @@
 /*global describe, beforeEach, module, inject, it, expect*/
 describe('directive: order-icon', function() {
   'use strict';
-  var element, scope;
+  var element, elementAlternate, scope;
   const orderType = 'type';
 
   beforeEach(module('dataDealer'));
 
   beforeEach(inject(function($rootScope, $compile, $controller) {
     scope = $rootScope.$new();
-
-    element = '<order-icon order-on="{{ type }}"></order-icon>';
-
     scope.DeckEditor = $controller('DeckEditorController');
 
-    scope.type = orderType;
+    element = '<order-icon order-on="' + orderType + '"></order-icon>';
     element = $compile(element)(scope);
+
+    elementAlternate = '<order-icon order-on="' + scope.DeckEditor.orderProp + '"></order-icon>';
+    elementAlternate = $compile(elementAlternate)(scope);
+
+    scope.$digest();
   }));
 
   it('should have no classes if ordering is not on the order-on attribute', function() {
@@ -39,5 +41,30 @@ describe('directive: order-icon', function() {
     expect(classList).to.contain('glyphicon-chevron-down');
 
     expect(classList).to.not.contain('glyphicon-chevron-up');
+  });
+
+  it('should show an up arrow if ordering is descending against the order-on attribute', function() {
+    scope.$apply(function() {
+      scope.DeckEditor.orderProp = orderType;
+      scope.DeckEditor.reverse = true;
+    });
+
+    var classList = element.find('span').attr('class');
+    classList = (classList ? classList + '' : '').split(' ');
+
+    expect(classList).to.contain('glyphicon');
+    expect(classList).to.contain('glyphicon-chevron-up');
+
+    expect(classList).to.not.contain('glyphicon-chevron-down');
+  });
+
+  it('should maintain separate scopes', function() {
+    var classList = elementAlternate.find('span').attr('class');
+    classList = (classList ? classList + '' : '').split(' ');
+
+    // In case this tests purpose is unclear, note that this is testing
+    // a different element to the others.
+    expect(classList).to.contain('glyphicon');
+    expect(classList).to.contain('glyphicon-chevron-down');
   });
 });
